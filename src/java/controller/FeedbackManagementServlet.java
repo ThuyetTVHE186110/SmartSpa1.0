41ea8599ff3c167ae3d1a61dbca21c4d9f86bb38
Ndchien2004 <144213919+Ndchien2004@users.noreply.github.com>
2024-10-24 08:42:17 +0700

    Big Update

--- src/java/controller/FeedbackManagementServlet.java
+++ src/java/controller/FeedbackManagementServlet.java
@@ -2,17 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import dal.FeedbackDAO;
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
+import model.Account;
 import model.Feedback;
 
 /**
@@ -20,31 +20,32 @@ import model.Feedback;
  * @author admin
  */
 public class FeedbackManagementServlet extends HttpServlet {
-    
 
-    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-     FeedbackDAO feedbackDAO = new FeedbackDAO();
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+
+        if (session == null || session.getAttribute("account") == null) {
+            response.sendRedirect("adminLogin.jsp");
+        } else {
+            // Lấy đối tượng account từ session
+            Account account = (Account) session.getAttribute("account");
+
+            if (account.getRole() == 1 || account.getRole() == 2 || account.getRole() == 3) {
+                request.getRequestDispatcher("dashboard.jsp").forward(request, response);
+            } else {
+                // Set error message and redirect to ErrorServlet
+                request.setAttribute("errorMessage", "You do not have the required permissions to access the dashboard.");
+                request.getRequestDispatcher("error").forward(request, response);
+            }
+        }
+        FeedbackDAO feedbackDAO = new FeedbackDAO();
         ArrayList<Feedback> feedback = feedbackDAO.getFeedback();
 
         request.setAttribute("feedback", feedback);
 
         request.getRequestDispatcher("feedback-management.jsp").forward(request, response);
-    } 
-
-   
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        
     }
-
-    
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
--- web/error.jsp
+++ web/error.jsp
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Registration Failed</title>
+        <title>Error 404</title>
+        <link rel="stylesheet" href="./css/screen.css">
     </head>
     <body>
-        <h2>Registration Failed</h2>
-        <p>${error}</p>
-        <a href="signup.jsp">Go back to sign up</a>
+        <h1>Oops! Page Not Found</h1>
+        <p class="zoom-area">The page you're looking for seems to have vanished like mascara in the rain. </p>
+        <section class="error-container">
+            <span class="four"><span class="screen-reader-text">4</span></span>
+            <span class="zero"><span class="screen-reader-text">0</span></span>
+            <span class="four"><span class="screen-reader-text">4</span></span>
+        </section>
+        <div class="link-container">
+            <a href="index" class="more-link">Return to Homepage</a>
+        </div>
     </body>
 </html>
