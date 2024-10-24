dc3ffeb8d762aee668bc763a148338fa34c9d612
Ndchien2004 <144213919+Ndchien2004@users.noreply.github.com>
2024-10-24 08:56:55 +0700

    Big Update 404

--- src/java/controller/FeedbackManagementServlet.java
+++ src/java/controller/FeedbackManagementServlet.java
@@ -6,13 +6,12 @@ package controller;
 
 import dal.FeedbackDAO;
 import java.io.IOException;
+import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
-import model.Account;
 import model.Feedback;
 
 /**
@@ -24,22 +23,6 @@ public class FeedbackManagementServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        HttpSession session = request.getSession(false);
-
-        if (session == null || session.getAttribute("account") == null) {
-            response.sendRedirect("adminLogin.jsp");
-        } else {
-            // Lấy đối tượng account từ session
-            Account account = (Account) session.getAttribute("account");
-
-            if (account.getRole() == 1 || account.getRole() == 2 || account.getRole() == 3) {
-                request.getRequestDispatcher("dashboard.jsp").forward(request, response);
-            } else {
-                // Set error message and redirect to ErrorServlet
-                request.setAttribute("errorMessage", "You do not have the required permissions to access the dashboard.");
-                request.getRequestDispatcher("error").forward(request, response);
-            }
-        }
         FeedbackDAO feedbackDAO = new FeedbackDAO();
         ArrayList<Feedback> feedback = feedbackDAO.getFeedback();
 
