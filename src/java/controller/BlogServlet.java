e1d17b5bb592b9efc9d243643a598aeb25ca0f4d
Ndchien2004 <144213919+Ndchien2004@users.noreply.github.com>
2024-10-27 22:26:32 +0700

    Big Update Blog

---
+++ src/java/controller/BlogServlet.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import dal.BlogDAO;
+import jakarta.servlet.RequestDispatcher;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+import model.Blog;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author PC
+ */
+@WebServlet(name = "BlogServlet", urlPatterns = {"/blog"})
+public class BlogServlet extends HttpServlet {
+
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        BlogDAO blogDAO = new BlogDAO();
+//        List<Blog> blogs;
+//        List<Blog> recentBlogs;
+//        int page = 1;
+//        int blogsPerPage = 4;
+//
+//        // Get the page number from the request, default to 1 if not provided
+//        String pageParam = request.getParameter("page");
+//        if (pageParam != null && !pageParam.isEmpty()) {
+//            page = Integer.parseInt(pageParam);
+//        }
+//
+//        try {
+//            int totalBlogs = blogDAO.getBlogCount(); // Method to get total number of blogs
+//            int totalPages = (int) Math.ceil((double) totalBlogs / blogsPerPage);
+//            // Fetch blogs for the current page
+//            blogs = blogDAO.getBlogsForPage(page, blogsPerPage);
+//            request.setAttribute("blogs", blogs);
+//            blogs = blogDAO.getAllBlogs();
+//            recentBlogs = blogDAO.getRecentBlogs();
+//            request.setAttribute("blogs", blogs);
+//            request.setAttribute("recentBlogs", recentBlogs);
+//            
+//            
+//            request.setAttribute("currentPage", page);
+//            request.setAttribute("totalPages", totalPages);
+//            
+//            
+//            request.getRequestDispatcher("blog.jsp").forward(request, response);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//            // Set an error message if an exception occurs
+//            request.setAttribute("error", "Unable to retrieve blog posts.");
+//            // Forward to error page or show an error message on the same page
+//            request.getRequestDispatcher("blog.jsp").forward(request, response);
+//        }
+//    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        BlogDAO blogDAO = new BlogDAO();
+        int page = 1;
+        int blogsPerPage = 4;
+
+        // Get the page number from the request, default to 1 if not provided
+        String pageParam = request.getParameter("page");
+        if (pageParam != null && !pageParam.isEmpty()) {
+            page = Integer.parseInt(pageParam);
+        }
+
+        try {
+            int totalBlogs = blogDAO.getBlogCount();
+            int totalPages = (int) Math.ceil((double) totalBlogs / blogsPerPage);
+
+            // Fetch blogs for the current page
+            List<Blog> blogs = blogDAO.getBlogsForPage(page, blogsPerPage);
+            List<Blog> recentBlogs = blogDAO.getRecentBlogs();
+            blogs = blogDAO.getAllBlogs();
+
+            request.setAttribute("blogs", blogs); // Set paginated blogs
+            request.setAttribute("recentBlogs", recentBlogs);
+            request.setAttribute("currentPage", page);
+            request.setAttribute("totalPages", totalPages);
+
+            request.getRequestDispatcher("blog.jsp").forward(request, response);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            request.setAttribute("error", "Unable to retrieve blog posts.");
+            request.getRequestDispatcher("blog.jsp").forward(request, response);
+        }
+    }
+
+}
