0479538b91fae4f5ad4630a701dc7a4a03ba1285
ChuongBui <chuongbdhe172784@fpt.edu.vn>
2024-11-02 00:43:38 +0700

    .

--- src/java/dal/CustomerDAO.java
+++ src/java/dal/CustomerDAO.java
@@ -4,10 +4,37 @@
  */
 package dal;
 
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import model.Person;
+
 /**
  *
  * @author admin
  */
-public class CustomerDAO {
-    
+public class CustomerDAO extends DBContext {
+
+    public CustomerDAO() {
+    }
+
+    public ArrayList<Person> getAllCustomer() {
+        ArrayList<Person> customerList = new ArrayList<>();
+        try {
+            String sql = "Select p.ID, p.Name, p.DateOfBirth, p.Gender, p.Phone, p.Email, p.Address, a.RoleID, a.Status  from Person p\n"
+                    + "  INNER JOIN Account a ON p.ID = a.PersonID\n"
+                    + "  WHERE a.RoleID = 4";
+            PreparedStatement statement = DBContext.getConnection().prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+            while(rs.next()){
+                
+            }
+        } catch (Exception e) {
+            
+        }
+
+        return null;
+
+    }
 }
